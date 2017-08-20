875088cbc844994806ddd37b477ce76595edcaee
stefanmanev <stef_manev@abv.bg>
2017-08-20 12:03:28 +0300

    adding firebase settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-
-import { PublicModule } from './public/public.module';
-import { PrivateWorkerModule } from './private-worker/private-worker.module';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import { appRoutes } from './app.routes';
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCPR3i_Jba33tqXYD-2_MynK9DhqZNy_XQ',
+  authDomain: 'maistore-maistore.firebaseapp.com',
+  databaseURL: 'https://maistore-maistore.firebaseio.com',
+  projectId: 'maistore-maistore',
+  storageBucket: 'maistore-maistore.appspot.com',
+  messagingSenderId: '639190553847'
+};
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes),
-    PublicModule,
-    PrivateWorkerModule
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
