bdce9c8a756b715ab2ce42bc1f06e7144b1a4a86
Emily <emilia.pavlova@gmail.com>
2017-08-24 23:28:15 +0300

    Revert "Issue 7 menu"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { Observable } from 'rxjs/Observable';
-import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -10,32 +6,5 @@ import * as firebase from 'firebase/app';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string;
-  user: Observable<firebase.User>;
-  items: FirebaseListObservable<any[]>;
-  msgVal = '';
-
-  constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
-    this.title = 'maistore-app';
-    this.items = af.list('/messages', {
-      query: {
-        limitToLast: 50
-      }
-    });
-
-    this.user = this.afAuth.authState;
-
-  }
-  login() {
-    this.afAuth.auth.signInAnonymously();
-  }
-
-  logout() {
-    this.afAuth.auth.signOut();
-  }
-
-  Send(desc: string) {
-    this.items.push({ message: desc });
-    this.msgVal = '';
-  }
+  title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireAuthModule } from 'angularfire2/auth';
+
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-// import { TabsModule } from 'ngx-bootstrap/tabs';
-
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-
-import { PublicModule } from './public/public.module';
-import { PrivateWorkerModule } from './private-worker/private-worker.module';
-
-import { appRoutes } from './app.routes';
-// import { appRoutes } from './app.routes';
-
-export const firebaseConfig = {
-  apiKey: 'AIzaSyCPR3i_Jba33tqXYD-2_MynK9DhqZNy_XQ',
-  authDomain: 'maistore-maistore.firebaseapp.com',
-  databaseURL: 'https://maistore-maistore.firebaseio.com',
-  projectId: 'maistore-maistore',
-  storageBucket: 'maistore-maistore.appspot.com',
-  messagingSenderId: '639190553847'
-};
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireDatabaseModule,
-    AngularFireAuthModule,
-    RouterModule.forRoot(appRoutes),
-    // TabsModule.forRoot(),
-    PublicModule,
-    PrivateWorkerModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
