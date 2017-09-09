e1af73b5fa30134b72ead991d9867e41257cb218
Emilia Pavlova <emilia.pavlova@gmail.com>
2017-09-09 14:56:14 +0300

    add worker and project details

--- src/app/public/workers/worker/worker.component.ts
+++ src/app/public/workers/worker/worker.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WorkerModel } from '../../../models/worker.model';
+import { WorkersService } from '../../../services/data/workers.service';
 
 @Component({
   selector: 'app-worker',
@@ -7,12 +9,19 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./worker.component.css']
 })
 export class WorkerComponent implements OnInit {
+  worker: WorkerModel;
   workerId: string;
+  // picUrl = '../../../../assets/worker.png';
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private workersService: WorkersService
+  ) { }
 
   ngOnInit() {
     this.workerId = this.route.snapshot.paramMap.get('workerId');
+    this.workersService.getById(this.workerId)
+      .subscribe(worker => this.worker = worker)
   }
 
 }
